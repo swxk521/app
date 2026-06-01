859938a4068ecc1ae3c2772c3c296b2ba6c6d5f8
守望 <61259428+swxk521@users.noreply.github.com>
2026-06-01 17:12:58 +0800

    Add files via upload

---
+++ luci-app-eqosplus/luasrc/controller/eqosplus.lua
@@ -0,0 +1,20 @@
+module("luci.controller.eqosplus", package.seeall)
+-- Copyright 2022-2023 sirpdboy <herboy2008@gmail.com>
+function index()
+    if not nixio.fs.access("/etc/config/eqosplus") then return end
+
+    local e = entry({"admin", "services", "eqosplus"}, cbi("eqosplus"), _("Eqosplus"), 10)
+    e.dependent=false
+    e.acl_depends = { "luci-app-eqosplus" }
+    entry({"admin", "services", "eqosplus", "status"}, call("act_status")).leaf = true
+end
+
+function act_status()
+    local sys  = require "luci.sys"
+    local uci  = require "luci.model.uci".cursor()
+    local enabled = uci:get("eqosplus", "@eqosplus[0]", "enabled") == "1"
+    local e = {} 
+    e.status = enabled and (sys.call(" busybox ps -w | grep eqosplus | grep -v grep  >/dev/null ") == 0)
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(e)
+end
